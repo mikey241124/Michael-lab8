822337f3501b7a44403aad80e5373b0a3d6cbb4f
mikey241124 <48805730+mikey241124@users.noreply.github.com>
2024-04-11 10:29:51 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 def encode(password):
     pass
 
+def main():
+    running = True
+    while running:
+        print('Menu')
+        print('-------------')
+        print('1. Encode')
+        print('2. Decode')
+        print('3. Quit')
+        print('')
+        user_in = input('Please enter an option: ')
+
 if __name__ == '__main__':
-    while True:
-        break
+    main()
