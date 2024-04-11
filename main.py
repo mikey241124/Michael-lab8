c90d07c7aa32493b61b696d53172d9635c29dbc3
mikey241124 <48805730+mikey241124@users.noreply.github.com>
2024-04-11 10:39:55 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,24 @@
+#Michael Salemi's Encode function
+
 def encode(password):
+    to_encode = 0
+    run = False
+    try:
+        to_encode = int(input('Please enter your password to encode: '))
+        run = True
+    except ValueError:
+        print('Please enter a valid password!')
+
+    if run:
+        password = str(password)
+        list = []
+        for i in range(0, len(to_encode)):
+            list.append(int(password[i:i+1]) + 3)
+
+
+
+
+def decode():
     pass
 
 def main():
@@ -10,7 +30,24 @@ def main():
         print('2. Decode')
         print('3. Quit')
         print('')
-        user_in = input('Please enter an option: ')
+
+        user_in = 0
+        try:
+            user_in = int(input('Please enter an option: '))
+        except ValueError:
+            pass
+
+        if user_in == 1:
+            encode()
+
+        if user_in == 2:
+            decode()
+
+        if user_in == 3:
+            running = False
+
+        else:
+            print("Please enter a valid input")
 
 if __name__ == '__main__':
     main()
